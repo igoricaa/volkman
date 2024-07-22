9063e8b88d070db1fec0765821f446b409591878
Igor Stanisavljevic <stanisavljevic.igor@proton.me>
2024-07-22 14:30:27 +0200

    projects grid/list tablet css, awards tablet css

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -2,54 +2,45 @@
 
 import Projects from '@/components/projects/Projects';
 import styles from './page.module.scss';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { ProjectsView } from '@/data/data';
 import ContactSection from '@/components/ContactSection';
 
 const Work = () => {
   const [view, setView] = useState<ProjectsView>('list');
-  const [isDesktop, setIsDesktop] = useState(false);
-
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      setIsDesktop(window.matchMedia('(min-width: 1024px)').matches);
-    }
-  }, [isDesktop]);
 
   return (
     <main className='main'>
       <div className={styles.pageHeader}>
         <h1>Work</h1>
-        {isDesktop && (
-          <div className={styles.previewButtonsWrapper}>
-            <button
-              className={[
-                styles.listView,
-                view === 'list' ? styles.active : '',
-              ].join(' ')}
-              onClick={() => {
-                setView('list');
-              }}
-            >
-              <div className={styles.bar}></div>
-              <div className={styles.bar}></div>
-            </button>
-            <button
-              className={[
-                styles.gridView,
-                view === 'grid' ? styles.active : '',
-              ].join(' ')}
-              onClick={() => {
-                setView('grid');
-              }}
-            >
-              <div className={styles.dot}></div>
-              <div className={styles.dot}></div>
-              <div className={styles.dot}></div>
-              <div className={styles.dot}></div>
-            </button>
-          </div>
-        )}
+        <div className={styles.previewButtonsWrapper}>
+          <button
+            className={[
+              styles.listView,
+              view === 'list' ? styles.active : '',
+            ].join(' ')}
+            onClick={() => {
+              setView('list');
+            }}
+          >
+            <div className={styles.bar}></div>
+            <div className={styles.bar}></div>
+          </button>
+          <button
+            className={[
+              styles.gridView,
+              view === 'grid' ? styles.active : '',
+            ].join(' ')}
+            onClick={() => {
+              setView('grid');
+            }}
+          >
+            <div className={styles.dot}></div>
+            <div className={styles.dot}></div>
+            <div className={styles.dot}></div>
+            <div className={styles.dot}></div>
+          </button>
+        </div>
       </div>
       <Projects view={view} isHome={false} />
 
