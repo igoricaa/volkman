91a8eb5e0e2b862d9c294ec20d34a77c5b56b24b
Igor Stanisavljevic <stanisavljevic.igor@proton.me>
2024-07-23 10:41:19 +0200

    connectedgrid refactoring claude

--- src/components/ConnectedGrid/ConnectedGrid.jsx
+++ src/components/ConnectedGrid/ConnectedGrid.jsx
@@ -11,20 +11,26 @@ import {
   projectsGalleryImageStyles,
   projectsGalleryImageStylesMobile,
 } from '@/data/data';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function ConnectedGrid({ gridContent, endingText }) {
   gsap.registerPlugin(ScrollTrigger);
   const [isMobile, setIsMobile] = useState(true);
 
+  useEffect(() => {
+    const handleResize = () => {
+      const isDesktop = window.matchMedia('(min-width: 1024px)').matches;
+      setIsMobile(!isDesktop);
+    };
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   useGSAP(() => {
-    const isDesktop = window.matchMedia('(min-width: 1024px)').matches;
-    setIsMobile(!isDesktop);
-
-    const grid = isDesktop
-      ? document.querySelector(`.${styles.gridDesktop}`)
-      : document.querySelector(`.${styles.gridMobile}`);
-
+    const grid = document.querySelector(`.${styles.grid}`);
     const gridItems = grid.querySelectorAll(`.${styles.gridItem}`);
 
     const initSmoothScrolling = () => {
@@ -53,21 +59,23 @@ export default function ConnectedGrid({ gridContent, endingText }) {
 
         const originX = isLeftSide ? 100 : 0;
 
-        gsap
-          .timeline({
-            defaults: {
-              duration: 1,
-              ease: 'power4',
-            },
-            scrollTrigger: {
-              trigger: item,
-              start: 'top bottom-=15%',
-              end: '+=100%',
-              scrub: true,
-            },
-          })
-          .fromTo(
-            item.querySelector(`.${styles.gridItemImg}`),
+        const tl = gsap.timeline({
+          defaults: {
+            duration: 1,
+            ease: 'power4',
+          },
+          scrollTrigger: {
+            trigger: item,
+            start: 'top bottom-=15%',
+            end: '+=100%',
+            scrub: true,
+          },
+        });
+
+        const imgElement = item.querySelector(`.${styles.gridItemImg}`);
+        if (imgElement) {
+          tl.fromTo(
+            imgElement,
             {
               scale: 0,
               transformOrigin: `${originX}% 0%`,
@@ -76,149 +84,113 @@ export default function ConnectedGrid({ gridContent, endingText }) {
               scale: 1,
             }
           );
-        // .fromTo(
-        //   item.querySelector(`.${styles.gridItemCaptionCustom}`),
-        //   {
-        //     xPercent: () => (isLeftSide ? 100 : -100),
-        //     opacity: 0,
-        //   },
-        //   {
-        //     ease: 'power4',
-        //     xPercent: 0,
-        //     opacity: 1,
-        //   },
-        //   0
-        // );
+        }
+
+        const captionElement = item.querySelector(
+          `.${styles.gridItemCaptionCustom}`
+        );
+        if (captionElement) {
+          tl.fromTo(
+            captionElement,
+            {
+              xPercent: () => (isLeftSide ? 100 : -100),
+              opacity: 0,
+            },
+            {
+              ease: 'power4',
+              xPercent: 0,
+              opacity: 1,
+            },
+            0
+          );
+        }
       });
     };
 
     initSmoothScrolling();
     scroll();
-  });
+  }, [isMobile]);
 
-  const desktopContent = gridContent.images.filter(
-    (image) => !image.mobileCaption
+  const renderGridItem = (image, index) => (
+    <figure
+      key={`projectImage${index}`}
+      className={`${styles.gridItem} ${
+        image.mobileCaption ? styles.gridItemText : ''
+      }`}
+      style={
+        isMobile
+          ? image.mobileCaption?.style ||
+            projectsGalleryImageStylesMobile[index]
+          : projectsGalleryImageStyles[index]
+      }
+    >
+      {image.src && (
+        <div className={styles.gridItemImg}>
+          <Image
+            src={image.src}
+            alt={gridContent.alt}
+            fill
+            style={{ objectFit: 'cover' }}
+            sizes={
+              isMobile && index === 6 ? '95vw' : isMobile ? '45vw' : '50vw'
+            }
+            priority
+          />
+        </div>
+      )}
+      {((!isMobile && image.caption) || (isMobile && image.mobileCaption)) && (
+        <figcaption
+          className={`${styles.gridItemCaptionCustom} ${
+            isMobile ? styles.mobileCaption : ''
+          }`}
+          style={
+            !isMobile && image.caption
+              ? {
+                  left: image.caption.style.left,
+                  textAlign: image.caption.style.textAlign,
+                }
+              : {}
+          }
+        >
+          <p>{isMobile ? image.mobileCaption?.text : image.caption?.text}</p>
+        </figcaption>
+      )}
+    </figure>
   );
 
   return (
-    <>
-      <div className={[styles.grid, styles.gridDesktop].join(' ')}>
-        {desktopContent.map((image, index) => (
-          <figure
-            key={`projectImage${index}`}
-            className={styles.gridItem}
-            style={
-              isMobile
-                ? projectsGalleryImageStylesMobile[index]
-                : projectsGalleryImageStyles[index]
-            }
+    <div
+      className={`${styles.grid} ${
+        isMobile ? styles.gridMobile : styles.gridDesktop
+      }`}
+    >
+      {gridContent.images
+        .filter((image) => {
+          if (!isMobile) {
+            return !image.mobileCaption;
+          }
+          return true;
+        })
+        .map((image, index) => renderGridItem(image, index))}
+      {endingText && (
+        <figure
+          className={styles.gridItem}
+          style={{
+            '--r': isMobile
+              ? gridContent.images.length + 1
+              : gridContent.images.filter((img) => !img.mobileCaption).length +
+                1,
+            '--c': isMobile ? 1 : 2,
+            '--s': isMobile ? 8 : 6,
+          }}
+        >
+          <figcaption
+            className={`${styles.gridItemCaptionCustom} ${styles.endingText}`}
           >
-            {image.src && (
-              <div className={styles.gridItemImg}>
-                <Image
-                  src={image.src}
-                  alt={gridContent.alt}
-                  fill
-                  style={{ objectFit: 'cover' }}
-                  sizes='(max-width: 1024px) 50vw, 50vw'
-                  priority
-                />
-              </div>
-            )}
-            {image.caption && (
-              <figcaption
-                className={styles.gridItemCaptionCustom}
-                style={{
-                  left: image.caption.style.left,
-                  textAlign: image.caption.style.textAlign,
-                }}
-              >
-                <p>{image.caption.text}</p>
-              </figcaption>
-            )}
-          </figure>
-        ))}
-        {endingText && (
-          <figure
-            className={styles.gridItem}
-            style={{
-              '--r': desktopContent.length + 1,
-              '--c': 2,
-              '--s': 6,
-            }}
-          >
-            <figcaption
-              className={[styles.gridItemCaptionCustom, styles.endingText].join(
-                ' '
-              )}
-            >
-              <p>{endingText}</p>
-            </figcaption>
-          </figure>
-        )}
-      </div>
-
-      <div className={[styles.grid, styles.gridMobile].join(' ')}>
-        {gridContent.images.map((image, index) => (
-          <figure
-            key={`projectImage${index}`}
-            className={[
-              styles.gridItem,
-              image.mobileCaption ? styles.gridItemText : null,
-            ].join(' ')}
-            style={
-              image.mobileCaption?.style
-                ? image.mobileCaption.style
-                : projectsGalleryImageStylesMobile[index]
-            }
-          >
-            {image.src && (
-              <div className={styles.gridItemImg}>
-                <Image
-                  src={image.src}
-                  alt={gridContent.alt}
-                  fill
-                  style={{ objectFit: 'cover' }}
-                  sizes={
-                    index === 6
-                      ? '(max-width: 1024px) 100vw, 50vw'
-                      : '(max-width: 1024px) 45vw, 50vw'
-                  }
-                  priority
-                />
-              </div>
-            )}
-            {image.mobileCaption && (
-              <figcaption
-                className={[
-                  styles.gridItemCaptionCustom,
-                  styles.mobileCaption,
-                ].join(' ')}
-              >
-                <p>{image.mobileCaption.text}</p>
-              </figcaption>
-            )}
-          </figure>
-        ))}
-        {endingText && (
-          <figure
-            className={styles.gridItem}
-            style={{
-              '--r': gridContent.images.length + 1,
-              '--c': isMobile ? 1 : 2,
-              '--s': isMobile ? 8 : 6,
-            }}
-          >
-            <figcaption
-              className={[styles.gridItemCaptionCustom, styles.endingText].join(
-                ' '
-              )}
-            >
-              <p>{endingText}</p>
-            </figcaption>
-          </figure>
-        )}
-      </div>
-    </>
+            <p>{endingText}</p>
+          </figcaption>
+        </figure>
+      )}
+    </div>
   );
 }
