5ac3f23b2ada2b162f4285768dfb0d58be0ca402
Igor Stanisavljevic <stanisavljevic.igor@proton.me>
2024-08-05 23:30:11 +0200

    mobile projects clickable

--- src/components/projects/Mobile/MobileProjects.tsx
+++ src/components/projects/Mobile/MobileProjects.tsx
@@ -1,6 +1,7 @@
 import { projectsFeatured } from '@/data/data';
 import Image from 'next/image';
 import styles from './MobileProjects.module.scss';
+import TransitionLink from '@/components/PageTransition/TransitionLink/TransitionLink';
 
 const MobileProjects = ({ isHome }: { isHome: boolean }) => {
   const projects = isHome ? projectsFeatured.slice(0, 5) : projectsFeatured;
@@ -10,17 +11,19 @@ const MobileProjects = ({ isHome }: { isHome: boolean }) => {
       {projects.map((project, index) => {
         return (
           <article key={`project_${index}`}>
-            <div className={styles.imageWrapper}>
-              <Image
-                src={`/projects/${project.featuredPhoto}`}
-                alt={project.title}
-                fill
-                sizes='(max-width: 1024px) 100vw, 0vw'
-                style={{ objectFit: 'cover' }}
-              />
-            </div>
-            <h3>{project.title}</h3>
-            <p>{project.category}</p>
+            <TransitionLink href={`/work/${project.slug}`}>
+              <div className={styles.imageWrapper}>
+                <Image
+                  src={`/projects/${project.featuredPhoto}`}
+                  alt={project.title}
+                  fill
+                  sizes='(max-width: 1024px) 100vw, 0vw'
+                  style={{ objectFit: 'cover' }}
+                />
+              </div>
+              <h3>{project.title}</h3>
+              <p>{project.category}</p>
+            </TransitionLink>
           </article>
         );
       })}
